9b08aac0a24d8cc253f47a51b19a102cf852f6bb
Bernardo Islas <104100982+bexmer@users.noreply.github.com>
2025-10-07 18:31:40 -0600

    Fix PWA configuration for Vercel

--- next.config.js
+++ next.config.js
@@ -1,13 +1,15 @@
 const withPWA = require("next-pwa")({
   dest: "public",
-  disable: process.env.NODE_ENV === "development", // Usamos esta versión, es más estándar
   register: true,
   skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Agrega esta línea si usas el componente Image de Next.js para imágenes externas
+  // images: { domains: ['images.unsplash.com'] },
 };
 
 module.exports = withPWA(nextConfig);
